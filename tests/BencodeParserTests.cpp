a54cf67c15cfce5805c08860b0ed4c44bf3ea728
Jonas Sasowski <sasowski@outlook.de>
2025-11-16 20:17:55 +0100

    Add first bencode parser tests

---
+++ tests/BencodeParserTests.cpp
@@ -0,0 +1,38 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest.h>
+
+#include "BencodeParser.hpp"
+
+using namespace bt;
+
+TEST_CASE("Parse integer 1") {
+    auto value = bencode::parse("i123e");
+    REQUIRE(std::holds_alternative<int64_t>(value));
+    REQUIRE(std::get<int64_t>(value) == 123);
+}
+
+TEST_CASE("Parse integer 2") {
+    auto value = bencode::parse("i-42e");
+    REQUIRE(std::holds_alternative<int64_t>(value));
+    REQUIRE(std::get<int64_t>(value) == -42);
+}
+
+TEST_CASE("Parse integer 3") {
+    auto value = bencode::parse("i0e");
+    REQUIRE(std::holds_alternative<int64_t>(value));
+    REQUIRE(std::get<int64_t>(value) == 0);
+}
+
+TEST_CASE("Parse integer overflow") {
+    REQUIRE_THROWS_AS(bencode::parse("i9223372036854775808e"), std::out_of_range);
+}
+
+TEST_CASE("Parse invalid integer") {
+    REQUIRE_THROWS_AS(bencode::parse("i12a3e"), std::invalid_argument);
+}
+
+TEST_CASE("Parse string") {
+    auto value = bencode::parse("4:spam");
+    REQUIRE(std::holds_alternative<std::string>(value));
+    REQUIRE(std::get<std::string>(value) == "spam");
+}
